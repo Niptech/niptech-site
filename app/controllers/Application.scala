e6e10f73121f92d98254a0581d189e27bcca0b2f
Fabrice Croiseaux <fcroiseaux@gmail.com>
2014-01-03 17:55:25 +0100

    Page caching and pre loading

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -3,38 +3,51 @@ package controllers
 import play.api._
 import play.api.mvc._
 import scala.concurrent.Future
+import play.api.Play.current
+import play.api.Play.{configuration => conf}
 
 import scala.concurrent.ExecutionContext.Implicits.global
 import wcm.WordpressAPI
 import play.api.libs.ws.WS
+import play.api.cache.Cached
 
 object Application extends Controller {
 
   def index = Action {
-    Ok(views.html.index(""))
+    implicit request =>
+      preloadEpisodesList
+      Ok(views.html.index(""))
   }
 
-  def displayEpisode(id: Int) = Action.async(
-    WordpressAPI.getEpisodeById(id).map {
-      res =>
-        Ok(views.html.episodeDetail(res \ "post"))
-    }
-  )
+  val port = conf.getString("http.port").getOrElse("80")
 
-  def episodeList(podcast: String, page: Int, count: Int) = Action.async(
-    WordpressAPI.getLatestEpisodes(podcast, page, count).map {
-      res =>
-        Ok(views.html.episodeList(res))
-    }
-  )
+  def preloadLink(link: String) = {
+    WS.url("http://localhost:" + port + link).get
+    ""
+  }
 
-  def test = Action.async(
-    WS.url("http://www.niptech.com/?json=get_post&id=3357")
-      .get
-      .map {
-      res =>
-        Ok(res.body)
-    }
-  )
+  def preloadEpisodesList = {
+    preloadLink("/niptech")
+    preloadLink("/nipdev")
+    preloadLink("/niplife")
+  }
+
+  def displayEpisode(id: Int) = Cached("detail/" + id, 60) {
+    Action.async(
+      WordpressAPI.getEpisodeById(id).map {
+        res =>
+          Ok(views.html.episodeDetail(res \ "post"))
+      }
+    )
+  }
+
+  def episodeList(podcast: String, page: Int, count: Int) = Cached(podcast + ":" + page + ":" + count, 60) {
+    Action.async(
+      WordpressAPI.getLatestEpisodes(podcast, page, count).map {
+        res =>
+          Ok(views.html.episodeList(res))
+      }
+    )
+  }
 
 }
